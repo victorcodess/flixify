3653b6840f97256f490bb5dd17c6554f7aa5d61e
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-07-21 18:01:04 +0100

    feat: build responsive search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
+import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="pt-16 sm:pt-28 lg:pt-0 flex justify-center items-start w-full">
+      {" "}
+      <div className="w-[140px] h-[100vh] hidden lg:block"></div>
+      <div className="lg:w-[100%] w-[100%] flex flex-col justify-center items-center lg:mt-14">
+        <SearchBar />
+      </div>
+    </main>
+  );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import search from "../public/assets/icon-search.svg";
+import Image from "next/image";
+
+const SearchBar = () => {
+  return (
+    <form className="w-[97%] sm:w-[95%]">
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <Image src={search} alt="search" className="w-[24px] sm:w-[32px]" />
+        </div>
+
+        <input
+          type="search"
+          id="default-search"
+          className="block w-[100%] p-4 pl-14 sm:pl-[70px] text-[16px] sm:text-[24px] text-white bg-transparent focus:outline-0 focus:border-b-2 border-[#161D2F] outline-none"
+          placeholder="Search for movies or TV series"
+          required
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
