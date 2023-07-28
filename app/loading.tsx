afd6ea1163632479bf9f012b275391ca7a5f1c8d
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-07-28 15:16:54 +0100

    feat: create loading UI component with animation

---
+++ app/loading.tsx
@@ -0,0 +1,41 @@
+import LoadingCard from "../components/LoadingCard";
+export default function Loading() {
+
+  return (
+    <main className="pt-[72px] sm:pt-28 lg:pt-0 flex justify-center items-start w-full">
+      {" "}
+      <div className="w-[140px] h-[100vh] hidden lg:block"></div>
+      <div className="lg:w-[87%] xl:w-[91%] w-[100%] flex flex-col justify-center items-center lg:mt-14">
+        <div
+          className="bg-[#161D2F] animate-pulse rounded-md w-[92%] sm:w-[96%] lg:w-[97%] h-[40px] sm:h-[53px]"
+          style={{
+            animationDuration: "1s",
+          }}
+        ></div>
+
+        <div className="flex flex-col items-start justify-center w-full gap-[16px] sm:gap-[25px] mt-[24px] mb-[61px] sm:mt-[34px] px-5">
+          <div
+            className="bg-[#161D2F] animate-pulse rounded-md w-[150px] h-[30px] sm:h-[48px]"
+            style={{
+              animationDuration: "1s",
+            }}
+          ></div>
+          <div className="grid w-[100%] grid-cols-2 grid-rows-1 place-content-center place-items-center gap-x-4 gap-y-4 sm:grid-cols-3 lg:grid-cols-4">
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+            <LoadingCard />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ components/LoadingCard.tsx
@@ -0,0 +1,29 @@
+const RegularCard = () => {
+  return (
+    <div className="flex-shrink-0 w-[100%]">
+      <div
+        className="rounded-lg animate-pulse bg-[#161D2F] w-full h-[110px] sm:h-[140px] lg:h-[174px]"
+        style={{
+          animationDuration: "1s",
+        }}
+      ></div>
+
+      <div className="flex flex-col gap-1 items-start justify-center sm:gap-1 mt-2">
+        <div
+          className="bg-[#161D2F] animate-pulse rounded-md w-[120px] h-[18px] sm:h-[19.5px]"
+          style={{
+            animationDuration: "1s",
+          }}
+        ></div>
+        <div
+          className="bg-[#161D2F] animate-pulse rounded-md w-[150px] h-[22.5px] sm:h-[27px]"
+          style={{
+            animationDuration: "1s",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default RegularCard;
