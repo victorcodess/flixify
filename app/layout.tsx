121c89cfda51a08416a96224a46d5522f45e6f2b
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-08-14 20:11:15 +0100

    perf: add SEO and og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,55 @@ import AuthContext from "@/context/AuthContext";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
+
+
 export const metadata: Metadata = {
   title: "Flixify",
   description: "Entertainment Web App",
+  generator: "Next.js",
+  colorScheme: "dark",
+  openGraph: {
+    title: "Flixify — Entertainment Web App",
+    description:
+      "Explore a vast collection of movies and TV series, bookmark your favorites, and enjoy seamless navigation across genres.",
+    url: "https://flixify.victorwilliams.me/",
+    siteName: "flixify.victorwilliams.me",
+    images: [
+      {
+        url: "https://github.com/victorcodess/flixify/assets/84178696/e7b1ffb1-498e-421e-a1b0-48092068ef0c",
+        width: 1200,
+        height: 630,
+        alt: "Flixify — Entertainment Web App",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Flixify — Entertainment Web App",
+    description:
+      "Explore a vast collection of movies and TV series, bookmark your favorites, and enjoy seamless navigation across genres.",
+    creator: "victorwill__",
+    creatorId: "1243720976552144897",
+    images: [
+      "https://github.com/victorcodess/flixify/assets/84178696/e7b1ffb1-498e-421e-a1b0-48092068ef0c",
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  category: "technology",
 };
 
 export default function RootLayout({
