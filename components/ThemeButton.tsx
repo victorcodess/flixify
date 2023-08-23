a2dec19d6decbb03d6516ed97bac4310ba68653b
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-08-23 15:48:30 +0100

    feat: add morph animation to theme toggle

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -2,10 +2,12 @@
 
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
-import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+// import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 const ThemeButton = () => {
   const { resolvedTheme, setTheme } = useTheme();
+  const [isDarkMode, setDarkMode] = useState(false);
 
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
@@ -17,18 +19,32 @@ const ThemeButton = () => {
     return null;
   }
 
+  const toggleDarkMode = () => {
+    setDarkMode((previous) => !previous);
+  };
+
   return (
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className="flex items-center justify-center rounded-lg"
+      className="flex toggle items-center justify-center rounded-lg"
       onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
     >
-      {resolvedTheme === "dark" ? (
+      {/* {resolvedTheme === "dark" ? (
         <SunIcon className="w-6 sm:w-7 dark:text-[#ffbf2a] hover:dark:text-[#96abd6]" />
       ) : (
         <MoonIcon className="w-6 sm:w-7 text-[#96abd6] hover:text-[#ffbf2a]" />
-      )}
+      )} */}
+
+      <DarkModeSwitch
+        style={{ transition: "nonee2xxwd" }}
+        className="toggle"
+        checked={isDarkMode}
+        onChange={toggleDarkMode}
+        size={28}
+        moonColor="#96abd6"
+        sunColor="#ffbf2a"
+      />
     </button>
   );
 };
